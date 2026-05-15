f7fc0889406e07377067eed5b1ea980f172e2a3f
Justin Reyes <justinreyes@Justins-MBP-2.localdomain>
2026-05-15 14:45:28 -0400

    Add deployment updates and output files

---
+++ build_push.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Build the docker image for the sufs manager app and push the newest version to the server/registry
+#For each update the sufs-manager.yaml file needs to be updated with the latest version number
+#test locally by creating environment and running
+#python3 main.py from app directory with modules modified for local use
+
+docker buildx build --push --platform linux/amd64 --tag registry.corzicsys.com/sufs-manager-web-app:v2 .
+cat sufs-manager.yaml | curl https://iolite.corzicsys.com/api/apps/sufs-manager -i -X PUT -H "Content-Type: text/yaml" -H "X-API-Key: XXn7tsQHbEqy4hHPQ2otg6OZA7FgbM" --data-binary @-
